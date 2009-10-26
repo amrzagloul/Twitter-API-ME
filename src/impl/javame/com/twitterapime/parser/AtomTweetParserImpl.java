46d76040ce486aae26b2ecc6289ed77562b8eaaa
ernandes@gmail.com <ernandes@gmail.com@bb74ebc7-4e7e-4bbf-a8e0-b75a88088e2a>
2009-10-26 00:47:16 +0000

    added javadoc and made some changes.

--- src/impl/javame/com/twitterapime/parser/AtomTweetParserImpl.java
+++ src/impl/javame/com/twitterapime/parser/AtomTweetParserImpl.java
@@ -27,6 +27,8 @@ import com.twitterapime.search.Tweet;
 
 /**
  * <p>
+ * This class defines the implementation of an Atom feed parser responsible for
+ * parsing the result from Twitter Search API, for Java ME platform. 
  * </p>
  * 
  * @author Ernandes Mourao Junior (ernandes@gmail.com)
@@ -63,16 +65,16 @@ public final class AtomTweetParserImpl extends FeedParser {
 					} else if (tag.equals("link")
 							&& xmlPath.equals("/feed/entry")) {
 						final String attrValue =
-							parser.getAttributeValue(0).toLowerCase();
+							getAttributeValue(parser, "type");
 						//
 						if (attrValue.equals("text/html")) {
 							tweetValues.put(
-								MetadataSet.TWEET_HTML_LINK,
-								parser.getAttributeValue(2));
+								MetadataSet.TWEET_URI,
+								getAttributeValue(parser, "href"));
 						} else if (attrValue.equals("image/png")) {
 							tweetValues.put(
-								MetadataSet.TWEET_PICTURE_LINK,
-								parser.getAttributeValue(2));
+								MetadataSet.TWEET_AUTHOR_PICTURE_URI,
+								getAttributeValue(parser, "href"));
 						}
 					}
 					//
@@ -93,19 +95,21 @@ public final class AtomTweetParserImpl extends FeedParser {
 					if (xmlPath.equals("/feed/entry/id")) {
 						tweetValues.put(MetadataSet.TWEET_ID, text);
 					} else if (xmlPath.equals("/feed/entry/published")) {
-						tweetValues.put(MetadataSet.TWEET_PUBLISHED, text);
+						tweetValues.put(
+							MetadataSet.TWEET_PUBLISH_DATE, formatDate(text));
 					} else if (xmlPath.equals("/feed/entry/title")) {
-						tweetValues.put(MetadataSet.TWEET_TITLE, text);
-					} else if (xmlPath.equals("/feed/entry/content")) {
 						tweetValues.put(MetadataSet.TWEET_CONTENT, text);
-					} else if (xmlPath.equals("/feed/entry/updated")) {
-						tweetValues.put(MetadataSet.TWEET_UPDATED, text);
 					} else if (xmlPath.equals("/feed/entry/twitter:source")) {
-						tweetValues.put(MetadataSet.TWEET_SOURCE, text);
+						tweetValues.put(MetadataSet.TWEET_SOURCE,
+								removeTags(text));
 					} else if (xmlPath.equals("/feed/entry/twitter:lang")) {
 						tweetValues.put(MetadataSet.TWEET_LANG, text);
 					} else if (xmlPath.equals("/feed/entry/author/name")) {
-						tweetValues.put(MetadataSet.TWEET_AUTHOR_NAME, text);
+						String[] names = splitAuthorNames(text);
+						tweetValues.put(
+							MetadataSet.TWEET_AUTHOR_USERNAME, names[0]);
+						tweetValues.put(
+							MetadataSet.TWEET_AUTHOR_NAME, names[1]);
 					} else if (xmlPath.equals("/feed/entry/author/uri")) {
 						tweetValues.put(MetadataSet.TWEET_AUTHOR_URI, text);
 					}
@@ -121,6 +125,106 @@ public final class AtomTweetParserImpl extends FeedParser {
 	
 	/**
 	 * <p>
+	 * Split the author's username and name from the given string.
+	 * </p>
+	 * @param name The name.
+	 * @return The username [0] and full name [1].
+	 */
+	private String[] splitAuthorNames(String name) {
+		String[] names = new String[2];
+		//
+		if (name == null) {
+			return names;
+		}
+		//
+		name = name.trim();
+		final int i = name.indexOf(' ');
+		if (i == -1) {
+			names[0] = name;
+			names[1] = name;
+		} else {
+			names[0] = name.substring(0, i);
+			names[1] = name.substring(i +2, name.length() -1);
+		}
+		//
+		return names;
+	}
+	
+	/**
+	 * <p>
+	 * Format the date of a tweet in standard date value.
+	 * </p>
+	 * @param date Tweet date.
+	 * @return Standard date value.
+	 */
+	private String formatDate(String date) {
+		return date != null
+			? date.replace('T', ' ').replace('Z', ' ').trim()
+			: null;
+	}
+	
+	/**
+	 * <p>
+	 * Get the value of a given attribute from the parser object.
+	 * </p>
+	 * @param parser The parser object.
+	 * @param attr The attribute.
+	 * @return The value.
+	 */
+	private String getAttributeValue(KXmlParser parser, String attr) {
+		for (int i = parser.getAttributeCount() -1; i >= 0; i--) {
+			if (parser.getAttributeName(i).equalsIgnoreCase(attr)) {
+				return parser.getAttributeValue(i);
+			}
+		}
+		//
+		return null;
+	}
+	
+	/**
+	 * <p>
+	 * Remove any tag occurrence from the given string.
+	 * </p>
+	 * @param str String to be parsed.
+	 * @return String with no tags.
+	 */
+	private String removeTags(String str) {
+		if (str == null) {
+			return null;
+		}
+		//
+		StringBuffer out = new StringBuffer();
+		char cs[] = str.toCharArray();
+		boolean tagFound = false;
+		int i1 = 0;
+		int l = 0;
+		//
+		for (int i = 0; i < cs.length; i++) {
+			if (cs[i] == '<' && !tagFound) {
+				out.append(cs, i1, l);
+				//
+				i1 = i;
+				l = 0;
+				tagFound = true;
+				l++;
+			} else if (cs[i] == '>' && tagFound) {
+				i1 = i +1;
+				l = 0;
+				tagFound = false;
+			} else {
+				l++;
+			}
+		}
+		if (l > 0) {
+			out.append(cs, i1, l);
+		}
+		//
+		return out.toString().trim();
+	}
+	
+	/**
+	 * <p>
+	 * This class implements a feed.
 	 * </p>
 	 * 
 	 * @author Ernandes Mourao Junior (ernandes@gmail.com)
@@ -129,10 +233,17 @@ public final class AtomTweetParserImpl extends FeedParser {
 	 */
 	private class TFeed extends DefaultEntity implements Feed {
 		/**
-		 * 
+		 * <p>
+		 * Hold the feed's entries.
+		 * </p>
 		 */
 		private Vector entries;
 		
+		/**
+		 * <p>
+		 * Create an instance of TFeed class.
+		 * </p>
+		 */
 		public TFeed() {
 			entries = new Vector(20);
 			data = new Hashtable();
@@ -160,24 +271,4 @@ public final class AtomTweetParserImpl extends FeedParser {
 			entries.addElement(entry);
 		}
 	}
-	
-//	public static void main(String[] args) {
-//		AtomTweetParserImpl p = new AtomTweetParserImpl();
-//		try {
-//			p.setFeedParserListener(new FeedParserListener() {
-//				public void feedParsed(Feed feed) {
-//					System.out.println(feed.toString());
-//				}
-//				
-//				public void feedEntryParsed(FeedEntry entry) {
-//					System.out.println(entry.toString());
-//				}
-//			});
-//			p.parse(AtomTweetParserImpl.class.getResourceAsStream("/atom-teste.xml"));
-//		} catch (IOException e) {
-//			e.printStackTrace();
-//		} catch (FeedParserException e) {
-//			e.printStackTrace();
-//		}
-//	}
 }
