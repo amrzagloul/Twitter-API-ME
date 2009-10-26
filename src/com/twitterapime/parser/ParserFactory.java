4a1143077e8abc9a2190b6a56f287a0f31e16054
ernandes@gmail.com <ernandes@gmail.com@bb74ebc7-4e7e-4bbf-a8e0-b75a88088e2a>
2009-10-26 00:45:56 +0000

    added javadoc and changed the get methods.

--- src/com/twitterapime/parser/ParserFactory.java
+++ src/com/twitterapime/parser/ParserFactory.java
@@ -12,6 +12,12 @@ import com.twitterapime.platform.PlatformProviderSelector;
 
 /**
  * <p>
+ * This is factory class for creating new parser objects. 
+ * </p>
+ * <p>
+ * The creation of a parser object is performed dynamically by looking up the
+ * current platform provider from PlatformProviderSelector class. For each
+ * supported platform, there is a specific implementation class.
  * </p>
  * 
  * @author Ernandes Mourao Junior (ernandes@gmail.com)
@@ -20,44 +26,71 @@ import com.twitterapime.platform.PlatformProviderSelector;
  */
 public final class ParserFactory {
 	/**
-	 * @return
+	 * <p>
+	 * Get an instance of a parser responsible for parsing a web feed.
+	 * </p>
+	 * @return The web feed parser object.
 	 */
 	public static FeedParser getDefaultFeedParser() {
-		final String JAVA_ME_FEED_PARSER_IMPL_CLASS =
-			"impl.javame.com.twitterapime.parser.AtomTweetParserImpl";
-		//
-		PlatformProvider p = PlatformProviderSelector.getCurrentProvider();
-		FeedParser parser = null;
 		try {
-			if (p.getID() == PlatformProvider.PPID_JAVA_ME) {
-				parser = (FeedParser)
-					Class.forName(JAVA_ME_FEED_PARSER_IMPL_CLASS).newInstance();
-			} else {
-				throw new IllegalArgumentException(
-					"Unknown platform ID: " + p.getID());
-			}
-		} catch (IllegalAccessException e) {
-		} catch (InstantiationException e) {
+			return (FeedParser)getParser(
+				Class.forName("com.twitterapime.parser.FeedParser"));
 		} catch (ClassNotFoundException e) {
+			return null;
 		}
-		//
-		return parser;
 	}
 	
 	/**
-	 * @return
+	 * <p>
+	 * Get an instance of a parser responsible for parsing the error messages
+	 * from Twitter API.
+	 * </p>
+	 * @return The error message parser object.
 	 */
-	public static ErrorMessageParser getDefaultErrorMessageParser(){
+	public static ErrorMessageParser getDefaultErrorMessageParser() {
+		try {
+			return (ErrorMessageParser)getParser(
+				Class.forName("com.twitterapime.parser.ErrorMessageParser"));
+		} catch (ClassNotFoundException e) {
+			return null;
+		}
+	}
+	
+	/**
+	 * <p>
+	 * Get the proper parser object for the parser class passed as argument
+	 * according to the current plataform provider.
+	 * </p>
+	 * @param parserClass Parser class object.
+	 * @return Proper parser object.
+	 */
+	private static Parser getParser(Class parserClass) {
+		final String JAVA_ME_FEED_PARSER_IMPL_CLASS =
+			"impl.javame.com.twitterapime.parser.AtomTweetParserImpl";
 		final String JAVA_ME_ERROR_MSG_PARSER_IMPL_CLASS =
 			"impl.javame.com.twitterapime.parser.XMLErrorMessageParserImpl";
 		//
 		PlatformProvider p = PlatformProviderSelector.getCurrentProvider();
-		ErrorMessageParser parser = null;
+		Parser parser = null;
+		//
 		try {
+			final Class FEED_PARSER_CLASS =
+				Class.forName("com.twitterapime.parser.FeedParser");
+			final Class ERROR_MSG_PARSER_CLASS =
+				Class.forName("com.twitterapime.parser.ErrorMessageParser");
+			//
+			//if JAVA ME PLATFORM
 			if (p.getID() == PlatformProvider.PPID_JAVA_ME) {
-				parser = (ErrorMessageParser)
-					Class.forName(
+				if (parserClass.equals(FEED_PARSER_CLASS)) {
+					parser = (Parser)Class.forName(
+						JAVA_ME_FEED_PARSER_IMPL_CLASS).newInstance();
+				} else if (parserClass.equals(ERROR_MSG_PARSER_CLASS)) {
+					parser = (Parser)Class.forName(
 						JAVA_ME_ERROR_MSG_PARSER_IMPL_CLASS).newInstance();
+				} else {
+					throw new IllegalArgumentException(
+						"Unknown parser class: " + parserClass.getName());
+				}
 			} else {
 				throw new IllegalArgumentException(
 					"Unknown platform ID: " + p.getID());
@@ -71,7 +104,9 @@ public final class ParserFactory {
 	}
 	
 	/**
-	 * 
+	 * <p>
+	 * Package-protected constructor to avoid object instantiation.
+	 * </p>
 	 */
 	ParserFactory() {
 	}
