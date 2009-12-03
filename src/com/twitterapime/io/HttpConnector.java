3b21a43e817bb3b1b34b0bb9b14cfa700f40a224
ernandes@gmail.com <ernandes@gmail.com@bb74ebc7-4e7e-4bbf-a8e0-b75a88088e2a>
2009-12-03 00:44:34 +0000

    - added the method encodeBase64() and newInstance() - updated the method open() to add Android support.

--- src/com/twitterapime/io/HttpConnector.java
+++ src/com/twitterapime/io/HttpConnector.java
@@ -14,7 +14,7 @@ import com.twitterapime.platform.PlatformProviderSelector;
 
 /**
  * <p>
- * This is factory class for creating new HttpConnection objects. 
+ * This is factory class for creating new HttpConnection objects.
  * </p>
  * <p>
  * The creation of HttpConnection is performed dynamically by looking up the
@@ -27,7 +27,7 @@ import com.twitterapime.platform.PlatformProviderSelector;
  * </p>
  * 
  * @author Ernandes Mourao Junior (ernandes@gmail.com)
- * @version 1.0
+ * @version 1.1
  * @since 1.0
  * @see HttpConnection
  */
@@ -36,8 +36,9 @@ public final class HttpConnector {
 	 * <p>
 	 * Create and open a HttpConnection.
 	 * </p>
+	 * 
 	 * @param url The URL for the connection.
-	 * @return A new HttpConnection object. 
+	 * @return A new HttpConnection object.
 	 * @throws IOException If an I/O error occurs.
 	 */
 	public static HttpConnection open(String url) throws IOException {
@@ -48,38 +49,41 @@ public final class HttpConnector {
 		final String JAVA_ME_HTTP_IMPL_CLASS =
 			"impl.javame.com.twitterapime.io.HttpConnectionImpl";
 		final String JAVA_ME_HTTP_USER_AGENT =
-			"Twitter API ME/1.0 (compatible; Java ME; MIDP-2.0; CLDC-1.0)";
+			"Twitter API ME/1.1 (compatible; Java ME; MIDP-2.0; CLDC-1.0)";
+		final String ANDROID_HTTP_IMPL_CLASS =
+			"impl.android.com.twitterapime.io.HttpConnectionImpl";
+		final String ANDROID_HTTP_USER_AGENT =
+			"Twitter API ME/1.1 (compatible; Android 1.5)";
 		//
-		PlatformProvider p = PlatformProviderSelector.getCurrentProvider();
+		final long PPID = PlatformProviderSelector.getCurrentProvider().getID();
+		//
 		HttpConnection conn = null;
 		String userAgent = null;
-		try {
-			if (p.getID() == PlatformProvider.PPID_JAVA_ME) {
-				conn = (HttpConnection)
-					Class.forName(JAVA_ME_HTTP_IMPL_CLASS).newInstance();
-				userAgent = JAVA_ME_HTTP_USER_AGENT;
-			} else {
-				throw new IllegalArgumentException(
-					"Unknown platform ID: " + p.getID());
-			}
-		} catch (IllegalAccessException e) {
-		} catch (InstantiationException e) {
-		} catch (ClassNotFoundException e) {
+		//
+		if (PPID == PlatformProvider.PPID_JAVA_ME) {
+			conn = newInstance(JAVA_ME_HTTP_IMPL_CLASS);
+			userAgent = JAVA_ME_HTTP_USER_AGENT;
+		} else if (PPID == PlatformProvider.PPID_ANDROID) {
+			conn = newInstance(ANDROID_HTTP_IMPL_CLASS);
+			userAgent = ANDROID_HTTP_USER_AGENT;
+		} else {
+			throw new IllegalArgumentException("Unknown platform ID: " + PPID);
 		}
 		//
-		//#ifdef DEBUG
+		// #ifdef DEBUG
 		System.out.println(url);
-		//#endif
+		// #endif
 		conn.open(url);
 		conn.setRequestProperty("User-Agent", userAgent);
 		//
 		return conn;
 	}
-	
+
 	/**
 	 * <p>
 	 * Encode the given URL.
 	 * </p>
+	 * 
 	 * @param url The URL to be encoded.
 	 * @return An encoded URL.
 	 */
@@ -114,18 +118,18 @@ public final class HttpConnector {
 			case ';':
 				eURL.append("%3B");
 				break;
-//			case ':':
-//				eURL.append("%3A");
-//				break;
+			// case ':':
+			// eURL.append("%3A");
+			// break;
 			case '@':
 				eURL.append("%40");
 				break;
-//			case '&':
-//				eURL.append("%26");
-//				break;
-//			case '=':
-//				eURL.append("%3D");
-//				break;
+			// case '&':
+			// eURL.append("%26");
+			// break;
+			// case '=':
+			// eURL.append("%3D");
+			// break;
 			case '+':
 				eURL.append("%2B");
 				break;
@@ -135,12 +139,12 @@ public final class HttpConnector {
 			case ',':
 				eURL.append("%2C");
 				break;
-//			case '/':
-//				eURL.append("%2F");
-//				break;
-//			case '?':
-//				eURL.append("%3F");
-//				break;
+			// case '/':
+			// eURL.append("%2F");
+			// break;
+			// case '?':
+			// eURL.append("%3F");
+			// break;
 			case '%':
 				eURL.append("%25");
 				break;
@@ -167,9 +171,97 @@ public final class HttpConnector {
 
 	/**
 	 * <p>
-	 * Package-protected constructor to avoid object instantiation.
+	 * Encode a given string to Base 64.
+	 * </p>
+	 * <p>
+	 * <i>This implementation is adopted from Kenneth Ballard's HttpClient
+	 * package. Released under LGPL.</i>
+	 * </p>
+	 * @param d String to encode.
+	 * @return Base64 encoded string.
+	 */
+	public static String encodeBase64(String d) {
+		String c =
+			"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+
+		byte[] code = c.getBytes();
+		byte[] s = d.getBytes();
+
+		int x;
+		int y = d.length() - (d.length() % 3);
+
+		byte[] coded = new byte[4];
+		String dest = "";
+
+		for (x = 0; x < y; x += 3) {
+			coded[3] = code[s[x + 2] % 64];
+			coded[0] = code[s[x] >> 2];
+
+			coded[1] = new Integer((s[x] % 4) << 4).byteValue();
+			coded[1] += s[x + 1] >> 4;
+			coded[1] = code[coded[1]];
+
+			coded[2] = new Integer((s[x + 1] % 16) << 2).byteValue();
+			coded[2] += s[x + 2] / 64;
+			coded[2] = code[coded[2]];
+
+			dest += new String(coded);
+		}
+
+		x = y;
+
+		if (s.length % 3 == 0)
+			return dest;
+
+		if (s.length % 3 == 1) {
+			coded[2] = '=';
+			coded[3] = '=';
+
+			coded[0] = code[s[x] >> 2];
+			coded[1] = code[new Integer((s[x] % 4) << 4).byteValue()];
+
+			dest += new String(coded);
+		}
+
+		if (s.length % 3 == 2) {
+			coded[3] = '=';
+
+			coded[0] = code[s[x] >> 2];
+			coded[1] = new Integer((s[x] % 4) << 4).byteValue();
+			coded[1] += s[x + 1] >> 4;
+			coded[1] = code[coded[1]];
+
+			coded[2] = code[new Integer((s[x + 1] % 16) << 2).byteValue()];
+
+			dest += new String(coded);
+		}
+
+		return dest;
+	}
+	
+	/**
+	 * <p>
+	 * Create a new instance of a HttpConnection subclass.
+	 * </p>
+	 * @param className Subclass's full name.
+	 * @return New HttpConnection instance.
+	 */
+	private static HttpConnection newInstance(String className) {
+		try {
+			return (HttpConnection)Class.forName(className).newInstance();
+		} catch (IllegalAccessException e) {
+		} catch (InstantiationException e) {
+		} catch (ClassNotFoundException e) {
+		}
+		//
+		return null;
+	}
+
+	/**
+	 * <p>
+	 * Private constructor to avoid object instantiation.
 	 * </p>
 	 */
-	HttpConnector() {
+	private HttpConnector() {
 	}
 }
