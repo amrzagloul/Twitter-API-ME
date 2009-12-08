416b4aa6792e02b9928f0d2a5bbb46b56d8f59de
ernandes@gmail.com <ernandes@gmail.com@bb74ebc7-4e7e-4bbf-a8e0-b75a88088e2a>
2009-12-08 00:31:48 +0000

    Updates for 1.1 version.

--- src/com/twitterapime/search/Tweet.java
+++ src/com/twitterapime/search/Tweet.java
@@ -10,20 +10,23 @@ package com.twitterapime.search;
 import java.util.Hashtable;
 
 import com.twitterapime.model.DefaultEntity;
-import com.twitterapime.parser.FeedEntry;
+import com.twitterapime.model.MetadataSet;
+import com.twitterapime.rest.TweetER;
+import com.twitterapime.rest.UserAccount;
 
 /**
  * <p>
  * This class defines an entity that represents a Tweet. A tweet is a message
- * sent by an user to Twitter.
+ * posted by an user to Twitter.
  * </p>
  * 
  * @author Ernandes Mourao Junior (ernandes@gmail.com)
- * @version 1.0
+ * @version 1.1
  * @since 1.0
  * @see SearchDevice
+ * @see TweetER
  */
-public final class Tweet extends DefaultEntity implements FeedEntry {
+public final class Tweet extends DefaultEntity {
 	/**
 	 * <p>
 	 * Create an instance of Tweet class.
@@ -40,5 +43,27 @@ public final class Tweet extends DefaultEntity implements FeedEntry {
 	 */
 	public Tweet(Hashtable data) {
 		super(data);
+	}
+	
+	/**
+	 * <p>
+	 * Create an instance of Tweet class.
+	 * </p>
+	 * @param content Content (status).
+	 */
+	public Tweet(String content) {
+		Hashtable data = new Hashtable();
+		data.put(MetadataSet.TWEET_CONTENT, content);
+		setData(data);
+	}
+
+	/**
+	 * <p>
+	 * Get the user account.
+	 * </p>
+	 * @return User account.
+	 */
+	public UserAccount getUserAccount() {
+		return (UserAccount)data.get(MetadataSet.TWEET_USER_ACCOUNT);
 	}
 }
