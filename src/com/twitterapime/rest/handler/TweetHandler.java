baa1b8d764c98c87779fe6db9517237707183f88
ernandes <ernandes@bb74ebc7-4e7e-4bbf-a8e0-b75a88088e2a>
2010-04-16 00:06:56 +0000

    updated to populate the data of their respectives entities.

--- src/com/twitterapime/rest/handler/TweetHandler.java
+++ src/com/twitterapime/rest/handler/TweetHandler.java
@@ -11,9 +11,6 @@ import java.util.Hashtable;
 
 import com.twitterapime.model.MetadataSet;
 import com.twitterapime.parser.DefaultXMLHandler;
-import com.twitterapime.parser.ParserException;
-import com.twitterapime.rest.UserAccount;
-import com.twitterapime.search.Tweet;
 import com.twitterapime.util.StringUtil;
 
 /**
@@ -22,120 +19,31 @@ import com.twitterapime.util.StringUtil;
  * </p>
  * 
  * @author Ernandes Mourao Junior (ernandes@gmail.com)
- * @version 1.0
+ * @version 1.1
  * @since 1.1
  */
 public final class TweetHandler extends DefaultXMLHandler {
 	/**
 	 * <p>
-	 * Hash with user account values.
+	 * Populate the given hash according to the tags and their values
 	 * </p>
+	 * @param data Hash to be populated.
+	 * @param path XML path.
+	 * @param text Tag's text.
 	 */
-	private Hashtable userAccountValues = new Hashtable(20);
-
-	/**
-	 * <p>
-	 * Hash with tweet values.
-	 * </p>
-	 */
-	private Hashtable tweetValues = new Hashtable(10);
-	
-	/**
-	 * @see com.twitterapime.parser.DefaultXMLHandler#text(java.lang.String)
-	 */
-	public void text(String text) throws ParserException {
-		text = text.trim();
-		//
-		if (xmlPath.equals("/status/created_at")) {
-			tweetValues.put(
+	public void populate(Hashtable data, String path, String text) {
+		if (path.endsWith("/created_at")) {
+			data.put(
 				MetadataSet.TWEET_PUBLISH_DATE,
 				"" + StringUtil.convertTweetDateToLong(text));
-		} else if (xmlPath.equals("/status/id")) {
-			tweetValues.put(MetadataSet.TWEET_ID, text);
-		} else if (xmlPath.equals("/status/text")) {
-			tweetValues.put(MetadataSet.TWEET_CONTENT, text);
-		} else if (xmlPath.equals("/status/source")) {
-			tweetValues.put(
-				MetadataSet.TWEET_SOURCE, StringUtil.removeTags(text));
-		} else if (xmlPath.equals("/status/favorited")) {
-			tweetValues.put(MetadataSet.TWEET_FAVOURITE, text);
-		} else if (xmlPath.equals("/status/user/id")) {
-			userAccountValues.put(MetadataSet.USERACCOUNT_ID, text);
-		} else if (xmlPath.equals("/status/user/name")) {
-			userAccountValues.put(MetadataSet.USERACCOUNT_NAME, text);
-		} else if (xmlPath.equals("/status/user/screen_name")) {
-			userAccountValues.put(MetadataSet.USERACCOUNT_USER_NAME, text);
-		} else if (xmlPath.equals("/status/user/location")) {
-			userAccountValues.put(MetadataSet.USERACCOUNT_LOCATION, text);
-		} else if (xmlPath.equals("/status/user/description")) {
-			userAccountValues.put(MetadataSet.USERACCOUNT_DESCRIPTION, text);
-		} else if (xmlPath.equals("/status/user/profile_image_url")) {
-			userAccountValues.put(MetadataSet.USERACCOUNT_PICTURE_URI, text);
-		} else if (xmlPath.equals("/status/user/url")) {
-			userAccountValues.put(MetadataSet.USERACCOUNT_URL, text);
-		} else if (xmlPath.equals("/status/user/protected")) {
-			userAccountValues.put(MetadataSet.USERACCOUNT_PROTECTED, text);
-		} else if (xmlPath.equals("/status/user/followers_count")) {
-			userAccountValues.put(
-				MetadataSet.USERACCOUNT_FOLLOWERS_COUNT, text);
-		} else if (xmlPath.equals("/status/user/profile_background_color")) {
-			userAccountValues.put(
-				MetadataSet.USERACCOUNT_PROFILE_BACKGROUND_COLOR, text);
-		} else if (xmlPath.equals("/status/user/profile_text_color")) {
-			userAccountValues.put(
-				MetadataSet.USERACCOUNT_PROFILE_TEXT_COLOR, text);
-		} else if (xmlPath.equals("/status/user/profile_link_color")) {
-			userAccountValues.put(
-				MetadataSet.USERACCOUNT_PROFILE_LINK_COLOR, text);
-		} else if (xmlPath.equals("/status/user/friends_count")) {
-			userAccountValues.put(MetadataSet.USERACCOUNT_FRIENDS_COUNT, text);
-		} else if (xmlPath.equals("/status/user/created_at")) {
-			userAccountValues.put(
-				MetadataSet.USERACCOUNT_CREATE_DATE,
-				"" + StringUtil.convertTweetDateToLong(text));
-		} else if (xmlPath.equals("/status/user/favourites_count")) {
-			userAccountValues.put(
-				MetadataSet.USERACCOUNT_FAVOURITES_COUNT, text);
-		} else if (xmlPath.equals("/status/user/utc_offset")) {
-			userAccountValues.put(MetadataSet.USERACCOUNT_UTC_OFFSET, text);
-		} else if (xmlPath.equals("/status/user/time_zone")) {
-			userAccountValues.put(MetadataSet.USERACCOUNT_TIME_ZONE, text);
-		} else if (xmlPath.equals("/status/user/profile_background_image_url")){
-			userAccountValues.put(
-				MetadataSet.USERACCOUNT_PROFILE_BACKGROUND_IMAGE_URI, text);
-		} else if (xmlPath.equals("/status/user/statuses_count")) {
-			userAccountValues.put(MetadataSet.USERACCOUNT_TWEETS_COUNT, text);
-		} else if (xmlPath.equals("/status/user/notifications")) {
-			userAccountValues.put(MetadataSet.USERACCOUNT_NOTIFICATIONS, text);
-		} else if (xmlPath.equals("/status/user/verified")) {
-			userAccountValues.put(MetadataSet.USERACCOUNT_VERIFIED, text);
+		} else if (path.endsWith("/id")) {
+			data.put(MetadataSet.TWEET_ID, text);
+		} else if (path.endsWith("/text")) {
+			data.put(MetadataSet.TWEET_CONTENT, text);
+		} else if (path.endsWith("/source")) {
+			data.put(MetadataSet.TWEET_SOURCE, StringUtil.removeTags(text));
+		} else if (path.endsWith("/favorited")) {
+			data.put(MetadataSet.TWEET_FAVOURITE, text);
 		}
-	}
-	
-	/**
-	 * @see com.twitterapime.parser.DefaultXMLHandler#endDocument()
-	 */
-	public void endDocument() throws ParserException {
-		tweetValues.put(
-			MetadataSet.TWEET_USER_ACCOUNT, new UserAccount(userAccountValues));
-	}
-	
-	/**
-	 * <p>
-	 * Return the parsed tweet.
-	 * </p>
-	 * @return Tweet.
-	 */
-	public Tweet getParsedTweet() {
-		return new Tweet(tweetValues);
-	}
-	
-	/**
-	 * <p>
-	 * Load the parsed values into the given tweet.
-	 * </p>
-	 */
-	public void loadParsedTweet(Tweet tweet) {
-		tweet.setData(tweetValues);
 	}
 }
--- src/com/twitterapime/rest/handler/UserAccountHandler.java
+++ src/com/twitterapime/rest/handler/UserAccountHandler.java
@@ -11,9 +11,6 @@ import java.util.Hashtable;
 
 import com.twitterapime.model.MetadataSet;
 import com.twitterapime.parser.DefaultXMLHandler;
-import com.twitterapime.parser.ParserException;
-import com.twitterapime.rest.UserAccount;
-import com.twitterapime.search.Tweet;
 import com.twitterapime.util.StringUtil;
 
 /**
@@ -22,111 +19,63 @@ import com.twitterapime.util.StringUtil;
  * </p>
  * 
  * @author Ernandes Mourao Junior (ernandes@gmail.com)
- * @version 1.0
+ * @version 1.1
  * @since 1.1
  */
 public final class UserAccountHandler extends DefaultXMLHandler {
 	/**
 	 * <p>
-	 * Hash with user account values.
+	 * Populate the given hash according to the tags and their values
 	 * </p>
+	 * @param data Hash to be populated.
+	 * @param path XML path.
+	 * @param text Tag's text.
 	 */
-	private Hashtable userAccountValues = new Hashtable(20);
-
-	/**
-	 * <p>
-	 * Hash with last tweet values.
-	 * </p>
-	 */
-	private Hashtable lastTweetValues = new Hashtable(10);
-
-	/**
-	 * @see com.twitterapime.parser.DefaultXMLHandler#text(java.lang.String)
-	 */
-	public void text(String text) throws ParserException {
-		text = text.trim();
-		//
-		if (xmlPath.equals("/user/id")) {
-			userAccountValues.put(MetadataSet.USERACCOUNT_ID, text);
-		} else if (xmlPath.equals("/user/name")) {
-			userAccountValues.put(MetadataSet.USERACCOUNT_NAME, text);
-		} else if (xmlPath.equals("/user/screen_name")) {
-			userAccountValues.put(MetadataSet.USERACCOUNT_USER_NAME, text);
-		} else if (xmlPath.equals("/user/location")) {
-			userAccountValues.put(MetadataSet.USERACCOUNT_LOCATION, text);
-		} else if (xmlPath.equals("/user/description")) {
-			userAccountValues.put(MetadataSet.USERACCOUNT_DESCRIPTION, text);
-		} else if (xmlPath.equals("/user/profile_image_url")) {
-			userAccountValues.put(MetadataSet.USERACCOUNT_PICTURE_URI, text);
-		} else if (xmlPath.equals("/user/url")) {
-			userAccountValues.put(MetadataSet.USERACCOUNT_URL, text);
-		} else if (xmlPath.equals("/user/protected")) {
-			userAccountValues.put(MetadataSet.USERACCOUNT_PROTECTED, text);
-		} else if (xmlPath.equals("/user/followers_count")) {
-			userAccountValues.put(
-				MetadataSet.USERACCOUNT_FOLLOWERS_COUNT, text);
-		} else if (xmlPath.equals("/user/profile_background_color")) {
-			userAccountValues.put(
-				MetadataSet.USERACCOUNT_PROFILE_BACKGROUND_COLOR, text);
-		} else if (xmlPath.equals("/user/profile_text_color")) {
-			userAccountValues.put(
-				MetadataSet.USERACCOUNT_PROFILE_TEXT_COLOR, text);
-		} else if (xmlPath.equals("/user/profile_link_color")) {
-			userAccountValues.put(
-				MetadataSet.USERACCOUNT_PROFILE_LINK_COLOR, text);
-		} else if (xmlPath.equals("/user/friends_count")) {
-			userAccountValues.put(MetadataSet.USERACCOUNT_FRIENDS_COUNT, text);
-		} else if (xmlPath.equals("/user/created_at")) {
-			userAccountValues.put(
+	public void populate(Hashtable data, String path, String text) {
+		if (path.endsWith("/id")) {
+			data.put(MetadataSet.USERACCOUNT_ID, text);
+		} else if (path.endsWith("/name")) {
+			data.put(MetadataSet.USERACCOUNT_NAME, text);
+		} else if (path.endsWith("/screen_name")) {
+			data.put(MetadataSet.USERACCOUNT_USER_NAME, text);
+		} else if (path.endsWith("/location")) {
+			data.put(MetadataSet.USERACCOUNT_LOCATION, text);
+		} else if (path.endsWith("/description")) {
+			data.put(MetadataSet.USERACCOUNT_DESCRIPTION, text);
+		} else if (path.endsWith("/profile_image_url")) {
+			data.put(MetadataSet.USERACCOUNT_PICTURE_URI, text);
+		} else if (path.endsWith("/url")) {
+			data.put(MetadataSet.USERACCOUNT_URL, text);
+		} else if (path.endsWith("/protected")) {
+			data.put(MetadataSet.USERACCOUNT_PROTECTED, text);
+		} else if (path.endsWith("/followers_count")) {
+			data.put(MetadataSet.USERACCOUNT_FOLLOWERS_COUNT, text);
+		} else if (path.endsWith("/profile_background_color")) {
+			data.put(MetadataSet.USERACCOUNT_PROFILE_BACKGROUND_COLOR, text);
+		} else if (path.endsWith("/profile_text_color")){
+			data.put(MetadataSet.USERACCOUNT_PROFILE_TEXT_COLOR, text);
+		} else if (path.endsWith("/profile_link_color")){
+			data.put(MetadataSet.USERACCOUNT_PROFILE_LINK_COLOR, text);
+		} else if (path.endsWith("/friends_count")) {
+			data.put(MetadataSet.USERACCOUNT_FRIENDS_COUNT, text);
+		} else if (path.endsWith("/created_at")) {
+			data.put(
 				MetadataSet.USERACCOUNT_CREATE_DATE,
 				"" + StringUtil.convertTweetDateToLong(text));
-		} else if (xmlPath.equals("/user/favourites_count")) {
-			userAccountValues.put(
-				MetadataSet.USERACCOUNT_FAVOURITES_COUNT, text);
-		} else if (xmlPath.equals("/user/utc_offset")) {
-			userAccountValues.put(MetadataSet.USERACCOUNT_UTC_OFFSET, text);
-		} else if (xmlPath.equals("/user/time_zone")) {
-			userAccountValues.put(MetadataSet.USERACCOUNT_TIME_ZONE, text);
-		} else if (xmlPath.equals("/user/profile_background_image_url")) {
-			userAccountValues.put(
-				MetadataSet.USERACCOUNT_PROFILE_BACKGROUND_IMAGE_URI, text);
-		} else if (xmlPath.equals("/user/statuses_count")) {
-			userAccountValues.put(MetadataSet.USERACCOUNT_TWEETS_COUNT, text);
-		} else if (xmlPath.equals("/user/notifications")) {
-			userAccountValues.put(MetadataSet.USERACCOUNT_NOTIFICATIONS, text);
-		} else if (xmlPath.equals("/user/verified")) {
-			userAccountValues.put(MetadataSet.USERACCOUNT_VERIFIED, text);
-		} else if (xmlPath.equals("/user/status/created_at")) {
-			lastTweetValues.put(
-				MetadataSet.TWEET_PUBLISH_DATE,
-				"" + StringUtil.convertTweetDateToLong(text));
-		} else if (xmlPath.equals("/user/status/id")) {
-			lastTweetValues.put(MetadataSet.TWEET_ID, text);
-		} else if (xmlPath.equals("/user/status/text")) {
-			lastTweetValues.put(MetadataSet.TWEET_CONTENT, text);
-		} else if (xmlPath.equals("/user/status/source")) {
-			lastTweetValues.put(
-				MetadataSet.TWEET_SOURCE, StringUtil.removeTags(text));
-		} else if (xmlPath.equals("/user/status/favorited")) {
-			lastTweetValues.put(MetadataSet.TWEET_FAVOURITE, text);
+		} else if (path.endsWith("/favourites_count")) {
+			data.put(MetadataSet.USERACCOUNT_FAVOURITES_COUNT, text);
+		} else if (path.endsWith("/utc_offset")) {
+			data.put(MetadataSet.USERACCOUNT_UTC_OFFSET, text);
+		} else if (path.endsWith("/time_zone")) {
+			data.put(MetadataSet.USERACCOUNT_TIME_ZONE, text);
+		} else if (path.endsWith("/profile_background_image_url")){
+			data.put(MetadataSet.USERACCOUNT_PROFILE_BACKGROUND_IMAGE_URI,text);
+		} else if (path.endsWith("/statuses_count")) {
+			data.put(MetadataSet.USERACCOUNT_TWEETS_COUNT, text);
+		} else if (path.endsWith("/notifications")) {
+			data.put(MetadataSet.USERACCOUNT_NOTIFICATIONS, text);
+		} else if (path.endsWith("/verified")) {
+			data.put(MetadataSet.USERACCOUNT_VERIFIED, text);
 		}
-	}
-	
-	/**
-	 * @see com.twitterapime.parser.DefaultXMLHandler#endDocument()
-	 */
-	public void endDocument() throws ParserException {
-		userAccountValues.put(
-			MetadataSet.USERACCOUNT_LAST_TWEET, new Tweet(lastTweetValues));
-	}
-	
-	/**
-	 * <p>
-	 * Return the parsed user account.
-	 * </p>
-	 * @return User account.
-	 */
-	public UserAccount getParsedUserAccount() {
-		return new UserAccount(userAccountValues);
 	}
 }
