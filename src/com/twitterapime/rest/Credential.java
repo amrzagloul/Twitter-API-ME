3c38de871d9dc9a5b7bef144dde679f2dc01cc1f
ernandes <ernandes@bb74ebc7-4e7e-4bbf-a8e0-b75a88088e2a>
2010-06-08 02:03:31 +0000

    added the constructor that receives the xauth keys and the method hasXAuthCredentials().

--- src/com/twitterapime/rest/Credential.java
+++ src/com/twitterapime/rest/Credential.java
@@ -18,7 +18,7 @@ import com.twitterapime.model.MetadataSet;
  * </p>
  * 
  * @author Ernandes Mourao Junior (ernandes@gmail.com)
- * @version 1.0
+ * @version 1.1
  * @since 1.1
  * @see UserAccountManager
  */
@@ -32,6 +32,21 @@ public final class Credential extends DefaultEntity {
 	 * @throws IllegalArgumentException If username/password is empty or null.
 	 */
 	public Credential(String username, String password) {
+		this(username, password, null, null);
+	}
+	
+	/**
+	 * <p>
+	 * Create an instance of Credential class.
+	 * </p>
+	 * @param username Username.
+	 * @param password Password.
+	 * @param consumerKey Consumer key.
+	 * @param consumerSecret Consumer secret.
+	 * @throws IllegalArgumentException If username/password is empty or null.
+	 */
+	public Credential(String username, String password, String consumerKey,
+		String consumerSecret) {
 		if (username == null || (username = username.trim()).length() == 0) {
 			throw new IllegalArgumentException(
 				"Username must not be empty/null");
@@ -41,9 +56,19 @@ public final class Credential extends DefaultEntity {
 				"Password must not be empty/null");
 		}
 		//
-		Hashtable credtls = new Hashtable(2);
+		Hashtable credtls = new Hashtable(4);
 		credtls.put(MetadataSet.CREDENTIAL_USERNAME, username);
 		credtls.put(MetadataSet.CREDENTIAL_PASSWORD, password);
+		//
+		if (consumerKey != null
+				&& (consumerKey = consumerKey.trim()).length() > 0) {
+			credtls.put(MetadataSet.CREDENTIAL_CONSUMER_KEY, consumerKey);
+		}
+		if (consumerSecret != null
+				&& (consumerSecret = consumerSecret.trim()).length() > 0) {
+			credtls.put(MetadataSet.CREDENTIAL_CONSUMER_SECRET, consumerSecret);
+		}
+		//
 		setData(credtls);
 	}
 	
@@ -57,5 +82,25 @@ public final class Credential extends DefaultEntity {
 		return getString(MetadataSet.CREDENTIAL_USERNAME) +
 		       ':' +
 		       getString(MetadataSet.CREDENTIAL_PASSWORD);
+	}
+	
+	/**
+	 * <p>
+	 * Verify whether XAuth credentials were entered.
+	 * </p>
+	 * @return true credentials entered.
+	 */
+	boolean hasXAuthCredentials() {
+		String key = getString(MetadataSet.CREDENTIAL_CONSUMER_KEY);
+		if (key == null || key.trim().length() == 0) {
+			return false;
+		}
+		//
+		key = getString(MetadataSet.CREDENTIAL_CONSUMER_SECRET);
+		if (key == null || key.trim().length() == 0) {
+			return false;
+		} else {
+			return true;
+		}
 	}
 }
