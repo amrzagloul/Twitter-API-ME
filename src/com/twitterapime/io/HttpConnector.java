5fe1b7ea2442d6763423c26c5ebdaa80a752c37c
ernandes@gmail.com <ernandes@gmail.com@bb74ebc7-4e7e-4bbf-a8e0-b75a88088e2a>
2010-08-28 17:56:00 +0000

    added method related to blackberries.

--- src/com/twitterapime/io/HttpConnector.java
+++ src/com/twitterapime/io/HttpConnector.java
@@ -9,6 +9,13 @@ package com.twitterapime.io;
 
 import java.io.IOException;
 
+//#ifdef PP_RIM
+//@import net.rim.device.api.servicebook.ServiceBook;
+//@import net.rim.device.api.servicebook.ServiceRecord;
+//@import net.rim.device.api.system.CoverageInfo;
+//@import net.rim.device.api.system.WLANInfo;
+//#endif
+
 import com.twitterapime.platform.PlatformProvider;
 import com.twitterapime.platform.PlatformProviderSelector;
 
@@ -32,6 +39,90 @@ import com.twitterapime.platform.PlatformProviderSelector;
  * @see HttpConnection
  */
 public final class HttpConnector {
+	/**
+	 * <p>
+	 * Create a new instance of a HttpConnection subclass.
+	 * </p>
+	 * @param className Subclass's full name.
+	 * @return New HttpConnection instance.
+	 */
+	private static final HttpConnection newInstance(String className) {
+		try {
+			return (HttpConnection)Class.forName(className).newInstance();
+		} catch (IllegalAccessException e) {
+		} catch (InstantiationException e) {
+		} catch (ClassNotFoundException e) {
+		}
+		//
+		return null;
+	}
+
+	//#ifdef PP_RIM
+//@	/**
+//@	 * <p>
+//@	 * Get some specific connection parameters that must be appended to a URL
+//@	 * connection on BlackBerry devices. Those parameters work to indicate which
+//@	 * type of connection the device is using at that moment.
+//@	 * </p>
+//@	 * 
+//@	 * @return Connection parameters.
+//@	 */
+//@	private static final String getBlackBerryConnectionParams() {
+//@	    String connParams = "";
+//@	    //
+//@	    if (WLANInfo.getWLANState() == WLANInfo.WLAN_STATE_CONNECTED) {
+//@	        connParams = ";interface=wifi"; //Connected to a WiFi access point.
+//@	    } else {
+//@			int coverageStatus = CoverageInfo.getCoverageStatus();
+//@			ServiceRecord record = getWAP2ServiceRecord();
+//@			//
+//@			if (record != null
+//@					&& (coverageStatus & CoverageInfo.COVERAGE_DIRECT) == CoverageInfo.COVERAGE_DIRECT) {
+//@				// Have network coverage and a WAP 2.0 service book record
+//@				connParams = ";deviceside=true;ConnectionUID="
+//@						+ record.getUid();
+//@			} else if ((coverageStatus & CoverageInfo.COVERAGE_MDS) == CoverageInfo.COVERAGE_MDS) {
+//@				// Have an MDS service book and network coverage
+//@				connParams = ";deviceside=false";
+//@			} else if ((coverageStatus & CoverageInfo.COVERAGE_DIRECT) == CoverageInfo.COVERAGE_DIRECT) {
+//@				// Have network coverage but no WAP 2.0 service book record
+//@				connParams = ";deviceside=true";
+//@			} else if ((coverageStatus & CoverageInfo.COVERAGE_BIS_B) == CoverageInfo.COVERAGE_BIS_B) {
+//@				connParams = ";deviceside=false;ConnectionType=mds-public";
+//@			}
+//@	    }
+//@	    //
+//@	    return connParams;
+//@	}
+//@
+//@	/**
+//@	 * <p>
+//@	 * Reading the service record for available connection.
+//@	 * </p>
+//@	 * 
+//@	 * @return ServiceRecord Currently in use.
+//@	 */
+//@	private static final ServiceRecord getWAP2ServiceRecord() {
+//@	    String cid;
+//@	    String uid;
+//@		ServiceBook sb = ServiceBook.getSB();
+//@	    ServiceRecord[] records = sb.getRecords();
+//@	    //
+//@	    for (int i = records.length -1; i >= 0; i--) {
+//@	        cid = records[i].getCid().toLowerCase();
+//@	        uid = records[i].getUid().toLowerCase();
+//@	        //
+//@	        if (cid.indexOf("wptcp") != -1 &&
+//@	        		uid.indexOf("wifi") == -1 &&
+//@	        		uid.indexOf("mms") == -1) {
+//@	            return records[i];
+//@	        }
+//@	    }
+//@	    //
+//@	    return null;
+//@	}
+	//#endif
+
 	/**
 	 * <p>
 	 * Create and open a HttpConnection.
@@ -71,33 +162,16 @@ public final class HttpConnector {
 			throw new IllegalArgumentException("Unknown platform ID: " + PPID);
 		}
 		//
-		// #ifdef DEBUG
-		System.out.println(url);
-		// #endif
+		//#ifdef PP_RIM
+//@		url += getBlackBerryConnectionParams();
+		//#endif
+		//
 		conn.open(url);
 		conn.setRequestProperty("User-Agent", userAgent);
 		//
 		return conn;
 	}
-
-	/**
-	 * <p>
-	 * Create a new instance of a HttpConnection subclass.
-	 * </p>
-	 * @param className Subclass's full name.
-	 * @return New HttpConnection instance.
-	 */
-	private static HttpConnection newInstance(String className) {
-		try {
-			return (HttpConnection)Class.forName(className).newInstance();
-		} catch (IllegalAccessException e) {
-		} catch (InstantiationException e) {
-		} catch (ClassNotFoundException e) {
-		}
-		//
-		return null;
-	}
-
+	
 	/**
 	 * <p>
 	 * Private constructor to avoid object instantiation.
